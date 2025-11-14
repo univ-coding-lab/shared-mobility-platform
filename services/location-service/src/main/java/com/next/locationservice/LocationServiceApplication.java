c64659a5bab91ad4a2dca8398a7f03ba3be1d153
unifolio0 <boho82ha@gmail.com>
2025-11-14 23:42:50 +0900

    chore: 중간 커밋

--- services/location-service/src/main/java/com/next/locationservice/LocationServiceApplication.java
+++ services/location-service/src/main/java/com/next/locationservice/LocationServiceApplication.java
@@ -2,10 +2,15 @@ package com.next.locationservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 import org.springframework.kafka.annotation.EnableKafka;
 
-@SpringBootApplication(scanBasePackages = {"com.next.locationservice", "com.next.common.event"})
+@SpringBootApplication(
+    scanBasePackages = {"com.next.locationservice", "com.next.common.event"},
+    exclude = {DataSourceAutoConfiguration.class}
+)
 @EnableMongoRepositories(basePackages = "com.next.locationservice.repository")
 @EnableKafka
 public class LocationServiceApplication {
